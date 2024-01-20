fc222a25e752659c2b024836f319858d406b0c71
Kirill Taran <kirilltaran@hotmail.com>
2024-01-20 07:27:57 +0400

    Refactoring

--- data-extractor/src/main.py
+++ data-extractor/src/main.py
@@ -1,21 +1,43 @@
-from services.data_extraction_service import DataExtractionService
+import os
+import sparknlp
+import sys
+from models.message import Message
+from services.data_extractor import DataExtractor
 from services.file_service import FileService
+from services.language_detector import LanguageDetector
 from utils.text_normalizer import TextNormalizer
 from encoders.message_encoder import MessageEncoder
-import os
 
 
-input_file_path = 'data' + os.sep + 'unprocessed_set.json'
-output_file_path = 'data' + os.sep + 'processed_set.json'
+def detect_language(spark: sparknlp.SparkSession, messages: list[Message]) -> list[Message]:
+    language_detector = LanguageDetector(spark)
+    return language_detector.detect(messages)
+
+
+def get_messages(tg_messages) -> list[Message]:
+    text_normalizer = TextNormalizer()
+    data_extractor = DataExtractor(text_normalizer)
+    
+    return data_extractor.process(tg_messages)
+
+
+def get_spark() -> sparknlp.SparkSession:
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+    
+    return sparknlp.start()
+
+
+def get_tg_messages(file_service, path):
+    file_content = file_service.read(path)
+    return file_content['messages']
 
 
 file_service = FileService()
-file_content = file_service.read(input_file_path)
-messages = file_content['messages']
+tg_messages = get_tg_messages(file_service, sys.argv[1])
+messages = get_messages(tg_messages)
 
-text_normalizer = TextNormalizer()
-data_preparation_service = DataExtractionService(text_normalizer)
+spark = get_spark()
+messages = detect_language(spark, messages)
 
-results = data_preparation_service.prepare_messages(messages)
-
-file_service.write(output_file_path, results, MessageEncoder)
+file_service.write(messages, sys.argv[2], MessageEncoder)
--- data-extractor/src/services/file_service.py
+++ data-extractor/src/services/file_service.py
@@ -5,22 +5,27 @@ import os
 
 class FileService:
     def read(self, path: str):
-        absolute_path: str = self._buildPath(path)
+        absolute_path: str = self._build_path(path)
 
         with open(absolute_path, 'r', encoding=self.ENCODING) as input_file:
             return json.load(input_file)
         
     
-    def write(self, path: str, data, encoder=JSONEncoder) -> None:
-        absolute_path: str = self._buildPath(path)
+    def write(self, data, path='results.json', encoder=JSONEncoder) -> None:
+        absolute_path: str = self._build_path(path)
         
         with open(absolute_path, "w", encoding=self.ENCODING) as output_file:
             json.dump(data, output_file, cls=encoder, ensure_ascii=False, indent=4)
         
     
-    def _buildPath(self, path: str) -> str:
+    def _build_path(self, path: str) -> str:
         absolute_path = os.getcwd()
-        return os.path.join(absolute_path, path)
+        data_path = self._build_data_path(path)
+        return os.path.join(absolute_path, data_path)
+    
+
+    def _build_data_path(self, path) -> str:
+        return 'data' + os.sep + path
 
 
     ENCODING = 'UTF8'
